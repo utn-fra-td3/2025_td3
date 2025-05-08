6a1e6d16512e9bb060ffd43a9450c0976a5f5fea
fac05 <facuruder@gmail.com>
2025-05-07 21:13:45 -0300

    Me confundi de firmware.c. Quedo arreglado

--- 3_trabajos_practicos/tp1/firmware/firmware.c
+++ 3_trabajos_practicos/tp1/firmware/firmware.c
@@ -1,33 +1,61 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-/*ahora con la libreria delotro ejercicio*/
+
 #include "FreeRTOS.h"
 #include "task.h"
+#include "pico/cyw43_arch.h"
+
+#define DELAY 500
+
+// Creo el handle de ambas tareas (sus referencias)
+TaskHandle_t Handle_blinkON = NULL;
+TaskHandle_t Handle_blinkOFF = NULL;
 
 /**
  * @brief Tarea de inicializacion
  */
-void task_init(void *params) {
+ void task_init(void *params) {
     // Inicializacion de GPIO
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, true);
-    gpio_put(PICO_DEFAULT_LED_PIN, 0);
+    cyw43_arch_init();
+    //Para que arranque el led encendido, suspendo inicialmente la tarea que apaga el led
+    vTaskSuspend(Handle_blinkOFF);
     // Elimino la tarea para liberar recursos
     vTaskDelete(NULL);
 }
 
 /**
- * @brief Tarea de blinky de LED
+ * @brief Tarea que enciende el led
  */
-void task_blinky(void *params) {
-    
+void task_blinkON(void *params) {
     while(1) {
-        // Toggle
-        gpio_put(PICO_DEFAULT_LED_PIN, !gpio_get(PICO_DEFAULT_LED_PIN));
-        // Demora de ticks equivalentes a 500 ms
-        vTaskDelay(pdMS_TO_TICKS(500));
+        // Enciendo el led
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        // Demora de ticks equivalentes a DELAY ms
+        vTaskDelay(pdMS_TO_TICKS(DELAY));
+        // Activo la otra
+        vTaskResume(Handle_blinkOFF);
+        //Suspendo la tarea de apagado, y activo la otra
+        vTaskSuspend(NULL);
     }
 }
+
+/**
+ * @brief Tarea que apaga el led
+ */
+ void task_blinkOFF(void *params) {
+    
+    while(1) {
+        // Apago el led
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        // Demora de ticks equivalentes a DELAY ms
+        vTaskDelay(pdMS_TO_TICKS(DELAY));
+        // Activo la otra
+        vTaskResume(Handle_blinkON);
+        //Suspendo la tarea de encendido, y activo la otra
+        vTaskSuspend(NULL);
+    }
+}
+
 
 /**
  * @brief Programa principal
@@ -37,7 +65,8 @@ int main(void) {
 
     // Creacion de tareas
     xTaskCreate(task_init, "Init", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
-    xTaskCreate(task_blinky, "Blinky", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    xTaskCreate(task_blinkON, "BlinkON", configMINIMAL_STACK_SIZE, NULL, 1, &Handle_blinkON);
+    xTaskCreate(task_blinkOFF, "BlinkOFF", configMINIMAL_STACK_SIZE, NULL, 1, &Handle_blinkOFF);
 
     // Arranca el scheduler
     vTaskStartScheduler();
