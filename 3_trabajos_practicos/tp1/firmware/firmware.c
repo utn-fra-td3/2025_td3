f6f1869532f9a5f88f9425b94b09cb521b34aa96
Ricardo <rmc.nec@gmail.com>
2025-05-15 11:34:59 -0300

    Segunda consigna de TP1 v1.2

--- 3_trabajos_practicos/tp1/firmware/firmware.c
+++ 3_trabajos_practicos/tp1/firmware/firmware.c
@@ -4,53 +4,42 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-/**
- * @brief Tarea para encender el LED
- */
+TaskHandle_t handle_led_on = NULL;
+TaskHandle_t handle_led_off = NULL;
+
 void task_led_on(void *params) {
     while (1) {
-        // Encender el LED
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        // Mantener encendido por 1000 ms
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1); // Encender LED
+        vTaskDelay(pdMS_TO_TICKS(1000));               // 1000 ms encendido
+        vTaskResume(handle_led_off);                   // Reanuda tarea de apagado
+        vTaskSuspend(NULL);                            // Se suspende a sí misma
     }
 }
 
-/**
- * @brief Tarea para apagar el LED
- */
 void task_led_off(void *params) {
     while (1) {
-        // Apagar el LED
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        // Mantener apagado por 1500 ms
-        vTaskDelay(pdMS_TO_TICKS(1500));
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0); // Apagar LED
+        vTaskDelay(pdMS_TO_TICKS(1500));               // 1500 ms apagado
+        vTaskResume(handle_led_on);                    // Reanuda tarea de encendido
+        vTaskSuspend(NULL);                            // Se suspende a sí misma
     }
 }
 
-/**
- * @brief Tarea de inicializacion
- */
 void task_init(void *params) {
-    // Inicializacion de GPIO
     cyw43_arch_init();
-
-    // Elimino la tarea para liberar recursos
     vTaskDelete(NULL);
 }
 
-/**
- * @brief Programa principal
- */
 int main(void) {
     stdio_init_all();
 
-    // Creacion de tareas
     xTaskCreate(task_init, "Init", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
-    xTaskCreate(task_led_on, "LED On", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-    xTaskCreate(task_led_off, "LED Off", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    xTaskCreate(task_led_on, "LED On", configMINIMAL_STACK_SIZE, NULL, 1, &handle_led_on);
+    xTaskCreate(task_led_off, "LED Off", configMINIMAL_STACK_SIZE, NULL, 1, &handle_led_off);
 
-    // Arranca el scheduler
+    // Arranca solo la tarea de encendido, la de apagado se suspende inicialmente
+    vTaskSuspend(handle_led_off);
+
     vTaskStartScheduler();
     while (1);
 }
