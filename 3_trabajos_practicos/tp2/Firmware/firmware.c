25ad236df59cb7303df85134f0a3bc1ed8cfc066
Nahuel <nahuluciano@gmail.com>
2025-05-18 22:00:40 -0300

    Primer consigna TP2

--- 3_trabajos_practicos/tp2/Firmware/firmware.c
+++ 3_trabajos_practicos/tp2/Firmware/firmware.c
@@ -1,45 +1,72 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-
+#include "hardware/adc.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "queue.h"
 
-/**
- * @brief Tarea de inicializacion
- */
-void task_init(void *params) {
-    // Inicializacion de GPIO
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, true);
-    gpio_put(PICO_DEFAULT_LED_PIN, 0);
-    // Elimino la tarea para liberar recursos
-    vTaskDelete(NULL);
+/*Consigna 1: Hacer un program en FreeRTOS que lea datos de un sensosr de temperatura por medio de 
+* una entrada ADC y los muestre por consola. Se puede usar el sensor interno de la Raspberry Pico*/
+
+/*NOTA: Se debe agregar manualmente un offset de temperatura, para la cosigna
+*se tomo un offset de 24°C, por lo tanto la formula para convertir la tension
+*a temperatura sera:
+* temperatura: 24 - (0.706 - voltaje)/0.001721 */
+
+QueueHandle_t lectura_adc;
+
+void captura_datos(void *pvParameter)
+{  uint16_t value_adc; //variable local, solo sirve dentro de la tarea
+
+    adc_init(); //inicio el ADC
+    adc_set_temp_sensor_enabled(true); //activo el sensor interno de la Pico 2
+    adc_select_input(4); //es el sensor de temperatura interno, no es un pin
+
+    while(1)
+    {
+        value_adc=adc_read(); //lectura del ADC que esta conectado al sensor
+        if(xQueueSend(lectura_adc,&value_adc,pdMS_TO_TICKS(100)) != pdTRUE)
+        {
+            printf("Cola llena \n");
+        }
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+
 }
 
-/**
- * @brief Tarea de blinky de LED
- */
-void task_blinky(void *params) {
-    
-    while(1) {
-        // Toggle
-        gpio_put(PICO_DEFAULT_LED_PIN, !gpio_get(PICO_DEFAULT_LED_PIN));
-        // Demora de ticks equivalentes a 500 ms
-        vTaskDelay(pdMS_TO_TICKS(500));
+void impresion_temp(void *pvParameter)
+{   uint16_t value_adc; //variable local, solo sirve dentro de la tarea
+    float temp,voltaje;
+
+    while (1)
+    {  
+        if (xQueueReceive(lectura_adc,&value_adc,pdMS_TO_TICKS(100)) == pdTRUE)
+        {
+            voltaje = value_adc *( 3.3f / 4095.0);
+            temp = 24 - ( 0.706f - voltaje) / 0.001721f;
+            printf("ADC:%d, Voltaje:%.2f V, Temperatura:%.2f °C\n",value_adc,voltaje,temp);
+        }  
     }
 }
 
-/**
- * @brief Programa principal
- */
-int main(void) {
+int main(void) 
+{
     stdio_init_all();
-
-    // Creacion de tareas
-    xTaskCreate(task_init, "Init", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
-    xTaskCreate(task_blinky, "Blinky", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-
-    // Arranca el scheduler
+    //Creacion de cola
+    lectura_adc = xQueueCreate(5,sizeof(uint16_t));
+    if(lectura_adc == NULL)
+    {
+        while (1)
+        {
+            printf("No se puedo crear la cola \n");
+        }
+        
+    }
+    //Creacion de tareas
+    xTaskCreate(captura_datos,"Emisora",256,NULL,1,NULL);
+    xTaskCreate(impresion_temp,"Receptora",256,NULL,1,NULL);
+    //inicio
     vTaskStartScheduler();
+
     while(1);
 }
