d90d797b9caf93f51964a57386756d820b90d3fc
AleeGallo <alejandro.m.gallo@hotmail.com>
2025-05-21 20:22:14 -0300

    Segunda consigna de TP2

--- 3_trabajos_practicos/tp2/firmware/firmware.c
+++ 3_trabajos_practicos/tp2/firmware/firmware.c
@@ -13,37 +13,68 @@
 // Cola para mandar numeros aleatorios
 QueueHandle_t q_adc;
 
+/**
+ * @brief Estructura para pasar los datos del sensor
+ */
+typedef struct {
+    uint16_t raw;
+    float voltage;
+    float temperature;
+} sensor_data_t;
+
+/**
+ * @brief Handler para la interrupcion del ADC
+ */
+void adc_irq_handler(void) {
+    // Variable para verificar la necesidad de un cambio tarea
+    BaseType_t to_higher_priority_task = false;
+    // Deshabilito la interrupcion y detengo el ADC
+    adc_irq_set_enabled(false);
+    adc_run(false);
+    // Variable para calcular el promedio de muestras
+    uint32_t raw = 0;
+    for(uint8_t i = 0; i < 4; i++) { raw += adc_fifo_get(); }
+    // Limpio el FIFO
+    adc_fifo_drain();
+
+    // Extraigo datos del promedio para enviar temperatura por la Cola
+    sensor_data_t data = { .raw = (uint16_t) (raw / 4) };
+    data.voltage = data.raw * 3.3 / (1 << 12);
+    data.temperature = 27 - (data.voltage - 0.706) / 0.001721;
+    
+    // Envio por cola
+    xQueueOverwriteFromISR(q_adc, &data.temperature, &to_higher_priority_task);
+    
+    // CAMBIO DE CONTEXTO
+    // Reviso si es necesario el cambio a otra tarea y salgo de interrupción
+    portYIELD_FROM_ISR(to_higher_priority_task);
+}
 
 /**
  * @brief Tarea que escribe por consola lo que llega
  * de la cola
  */
 void task_print(void *params) {
-
+    float temp_C;
     while(1) {
-        // Bloqueo tarea hasta que llegue el dato
-        float temp_C;
-        xQueueReceive(q_adc, &temp_C, portMAX_DELAY);
+        // Uso peek para lectura
+        xQueuePeek(q_adc, &temp_C, portMAX_DELAY);
         // Escribo por consola
         printf("Temperatura: %.2fC\n", temp_C);
-
+        vTaskDelay(500);
     }
 }
 
 void task_adc(void *params) {
-    uint16_t raw;
-    float voltaje;
-    float temp_C;
     while (1)
     {
-        raw = adc_read();
-        voltaje = 3.3 * raw / (1<<12);
-        temp_C = 27 - (voltaje - 0.706) / 0.001721;
-        xQueueSend(q_adc, &temp_C, portMAX_DELAY);
+        // Habilito interrupcion del ADC y corro las conversiones
+        adc_irq_set_enabled(true);
+        adc_run(true);
         vTaskDelay(500);
     }
-    
 }
+
 
 
 /**
@@ -58,8 +89,19 @@ int main(void) {
 
     q_adc = xQueueCreate (1, sizeof(float));
 
-    // Creacion de tareas
-    xTaskCreate(task_print, "Print", 2 * configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    // Inicializo la interrupcion del ADC y la cantidad de lecturas necesarias
+    adc_fifo_setup(true, false, 4, false, false);
+    adc_irq_set_enabled(true);
+
+    // Seteo el Handler (función void para tratar la interrupción)
+    irq_set_exclusive_handler(ADC_IRQ_FIFO, adc_irq_handler);
+
+    // Habilito interrupcion del ADC y corro las conversiones
+    irq_set_enabled(ADC_IRQ_FIFO, true);
+    adc_run(true);
+
+    /* Creacion de tareas */ 
+    xTaskCreate(task_print, "Print", 2 * configMINIMAL_STACK_SIZE, NULL, 2, NULL);
     xTaskCreate(task_adc, "ADC", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 
     // Arranca el scheduler
