80fc23821b6265399ea5cfa21fe5f166b24b1a9e
Ignacio Martinez <ignacio.martinez.03@gmail.com>
2025-05-21 20:23:31 -0300

    Segunda Consigna TP2

--- 3_trabajos_practicos/tp2/firmware/firmware.c
+++ 3_trabajos_practicos/tp2/firmware/firmware.c
@@ -14,10 +14,10 @@ QueueHandle_t q_adc;
 // DEFINO LA ESTRUCTURA DE LECTURA
 
 typedef struct {
-    uint16_t raw;               //  FORMATO DE 2 BYTES
+    uint32_t raw;               //  FORMATO DE 2 BYTES
     float voltage;              //  FORMATO PUNTO FLOTANTE
     float temperature;          //  FORMATO PUNTO FLOTANTE
-} sensor_data;
+}sensor_data;
 
 
 // DEFINO LAS FUNCIONES
@@ -29,28 +29,33 @@ void adc_irq_handler(void) {
     adc_irq_set_enabled(false);                          // PARO LA INTERRUPCON
     adc_run(false);                                      // DESHABILITO EL ADC
     
-    uint32_t adc_value = 0;                              // VARIABLE PARA PROMEDIO
+    sensor_data adc_value;                              // VARIABLE PARA PROMEDIO
+    adc_value.raw = 0;                                  // INICIALIZO EN 0
 
     for(uint8_t i = 0; i < SAMPLES; i++)                 
         { 
-            adc_value += adc_fifo_get();                  // LEO EL FIFO PARA LA CANTIDAD DE MUESTRAS QUE ELEGI
+            adc_value.raw += adc_fifo_get();                  // LEO EL FIFO PARA LA CANTIDAD DE MUESTRAS QUE ELEGI
         }
 
     adc_fifo_drain();                                     // LIMPIO EL FIFO
 
-    xQueueOverwriteFromISR(q_adc, &data, &to_higher_priority_task);      // ENVIO EL DATO A LA COLA
+    xQueueOverwriteFromISR(q_adc, &adc_value, &to_higher_priority_task);      // ENVIO EL DATO A LA COLA
 }
 
 void task_print_temp(void *params)
 {
-    sensor_data data = {0};                                   //  DEFINO ESTRUCTURA PARA LA COLA E INICIALIZO EN 0
+    sensor_data data;                                   //  DEFINO ESTRUCTURA PARA LA COLA E INICIALIZO EN 0
+    data.raw = 0;
+
     while (1)
     {
-        xQueuePeek(q_adc, &data, portMAX_DELAY);                        //  LEO EL ULTIMO VALOR EN LA COLA
+        xQueueReceive(q_adc, &data, portMAX_DELAY);                        //  LEO EL ULTIMO VALOR EN LA COLA
 
-        sensor_data data = { .raw = (uint16_t) (raw / SAMPLES) };       // CALCULO DE TEMPERATURA
+        data.raw = (uint32_t) (data.raw / SAMPLES);                     // CALCULO DE TEMPERATURA
         data.voltage = data.raw * 3.3 / (1 << 12);                      // CALCULO DE TEMPERATURA
         data.temperature = 27 - (data.voltage - 0.706) / 0.001721;      // CALCULO DE TEMPERATURA
+
+        data.temperature = 
 
         printf("Temperatura: %.2f C\n\n", data.temperature);    //  ESCRIBO EL VALOR EN LA PANTALLA
     }
@@ -65,8 +70,8 @@ void task_init(void *params) {
 
     adc_fifo_setup(true, false, SAMPLES, false, false);          // SETEO EL FIFO
     adc_irq_set_enabled(true);                                   // ACTIVO LA INTERRUPCIN DEL ADC
-    irq_set_exclusive_handler(ADC_IRQ_FIFO, adc_irq_handler);    // CONSULTAR
-    irq_set_enabled(ADC_IRQ_FIFO, true);                         // CONSULTAR
+    irq_set_exclusive_handler(ADC_IRQ_FIFO, adc_irq_handler);    // LA INTERRUPCION LLAMA A LA FUNCION
+    irq_set_enabled(ADC_IRQ_FIFO, true);                         // HABILITA LA INTERRUPCION
     adc_run(true);                                               // PONGO A CORRER AL ADC
 
     q_adc = xQueueCreate(1, sizeof(sensor_data));                // INICIALIZO LA COLA
@@ -76,19 +81,13 @@ void task_init(void *params) {
 
 void task_adc(void *params) {
 
-    sensor_data data = {0};                                      // DEFINO ESTRUCTURA PARA LA COLA E INICIALIZO EN 0
+      while(1) {
+        
+        adc_irq_set_enabled(true);                          // INICIO LA INTERRUPCON
+        adc_run(true);                                      // HABILITO EL ADC
+        vTaskDelay(pdMS_TO_TICKS(500));                     // HAGO SALTAR LA CONVERSION DEL ADC
 
-    while(1) {
-  
-        data.raw = adc_read();                                   // LEO EL DATO DEL SENSOR
-
-        // CALCULO LA TEMPERATURA CON LA CONVERSION
-
-        data.voltage = data.raw * 3.3 / (1 << 12);
-        data.temperature = 27 - (data.voltage - 0.706) / 0.001721;
-  
-        xQueueOverwrite(q_adc, &data);                    // ESCRIBO EL VALOR AL FINAL DE LA COLA
-    }
+      }
 }
 
 int main(void) {
